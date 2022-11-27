5c225e0985380842a0a416eb3d05407912ba9426
BALLARI SRINIVAS <sps21042003@gmail.com>
2022-11-27 16:09:05 +0530

    blog site without DB

--- app.js
+++ app.js
@@ -1,55 +1,58 @@
 const express = require("express");
 const app = express();
-const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const lodashobj = require("lodash");
-app.set('search engine','ejs');
+const bodyParser = require("body-parser");
+const lodash = require("lodash");
+app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
-const homeContent = "ransfodf sidubf ssiudhf  sdbf b fsd fisydbfdhbfhdhfi  sdf.";
-const aboutContent = "this is the about content ..asjns dsiduf sd dfjs dfs ";
-const contactContent = "this is the contact content .. sdufh sn dhadjasdbfhad a";
-
+const aboutCont = "What is Lorem Ipsum Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
 
 var allPosts = [];
 
 app.get("/",function(req,res){
-    res.render("home.ejs",{receiveHC:homeContent, receiveAllPosts:allPosts});
+    res.render("home.ejs",{allPosts:allPosts});
 });
 
 app.get("/about",function(req,res){
-    res.render("about.ejs",{receiveAC:aboutContent});
+    res.render("about.ejs",{rAbtCnt:aboutCont});
 });
-
-app.get("/contact",function(req,res){
-    res.render("contact.ejs",{receiveCC:contactContent});
-})
 
 app.get("/compose",function(req,res){
     res.render("compose.ejs");
 });
 
-app.get("/posts/:topic",function(req,res){
-    let searchTopic = req.params.topic;
-    searchTopic = lodashobj.lowerCase(searchTopic);
-
-    allPosts.forEach(function(obj){
-        if(lodashobj.lowerCase(obj.postTitle) === searchTopic){
-            res.render("post.ejs",{receiveTitle:obj.postTitle , receiveText:obj.postText});
-        }
-    });
-}); 
-
 app.post("/compose",function(req,res){
     var postObj = {
-        postTitle : req.body.titleInput,
-        postText : req.body.postInput
+        title : req.body.title,
+        post : req.body.post
     };
     allPosts.push(postObj);
     res.redirect("/");
-})
+});
 
-app.listen(3000,function(){
-    console.log("server has started");
+app.post("/home",function(req,res){
+    var search = req.body.searchTitle;
+    search = lodash.lowerCase(search);
+    let flagCnt = 0;
+    allPosts.forEach(function(obj){
+        let title = obj.title;
+        if(lodash.lowerCase(title) === search){
+            res.render("post.ejs",{title : obj.title , post : obj.post});
+        }else{
+            flagCnt++;
+        }
+    });
+    if(flagCnt == allPosts.length){
+        res.render("error.ejs");
+    }
+});
+
+
+app.post("/error",function(req,res){
+    res.redirect("/");
+})
+app.listen(3000,function(req,res){
+    console.log("server started at port 3000");
 });
